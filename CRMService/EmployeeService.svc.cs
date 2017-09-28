287dfbfb6a96d83c07ed52b506c1ade8177d7f6e
meanprogrammer <mean.programmer.d@gmail.com>
2017-09-28 15:23:54 +0800

    vf

---
+++ CRMService/EmployeeService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CRMService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select EmployeeService.svc or EmployeeService.svc.cs at the Solution Explorer and start debugging.
+    public class EmployeeService : IEmployeeService
+    {
+        XrmServiceContext context = null;
+
+        public void DoWork()
+        {
+        }
+
+        public IEnumerable<new_employee> Employees()
+        {
+            return null;
+        }
+    }
+}
---
+++ CRMService/IEmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CRMService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployeeService" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        IEnumerable<new_employee> Employees();
+    }
+}
